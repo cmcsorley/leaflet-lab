94607b05e91e365212f44ed80f7b65a4aa4a5090
cmcsorley <cmcsorley@wisc.edu>
2019-02-20 15:10:07 -0600

    Add files via upload
    Module 5A completed

--- main.js
+++ main.js
@@ -5,43 +5,59 @@
 
 var mymap = L.map('mapid').setView([0, 0], 0.25);
 
-//add tile layer
+//1. add tile layer
 var l = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(mymap);
 
-function jQueryAjax(){
-    //basic jQuery ajax method
+//Step 3: Add circle markers for point features to the map
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 1/25000;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+    console.log(radius);
+};
+
+function createPropSymbols(data, map){
+    //create marker options
+    var attribute = "y1995";
+    
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            var attValue = Number(feature.properties[attribute]);
+            console.log(attValue);
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+//Step 2: Import GeoJSON data
+function getData(map){
+    //load the data
     $.ajax("data/LabData.geojson", {
         dataType: "json",
-        success: callback
+        success: function(response){
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
+        }
     });
 };
 
-//define callback function
-function callback(response, status, jqXHRobject){
-    //tasks using the data go here
-    console.log(response);
-    
-    //for each element, add a circle at the lat and long coordinates
-    
-    for(var i=0; i<20; i++){
-        var long = response.features[i].geometry.coordinates[0];
-        var lat = response.features[i].geometry.coordinates[1];
-        var circle = L.circle([lat, long], {
-            //specify outline and fill colors, opacity, radius
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.5,
-            radius: (response.features[i].properties.y1990)/1.5
-        }).addTo(mymap);
-}
 
-};
-
-$(document).ready(jQueryAjax);
-
-//a test to make sure everything ran
-console.log("Hello");
-
-$(document).ready(initialize);
+$(document).ready(getData(mymap));
