2bea1c6bdfd626e45327541199eae8e4cb5b60e4
cmcsorley <cmcsorley@wisc.edu>
2019-03-05 11:03:02 -0600

    Add files via upload
    Activity 5B: Leaflet Interaction Operators Completed

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 /* Javascript by Catherine McSorley, 2019 */
 /* Example from Leaflet Quick Start Guide*/
 
-var mymap = L.map('mapid').setView([0, 0], 0.25);
+var mymap = L.map('mapid').setView([20, 0], 1.25);
 
 //1. add tile layer
 var l = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -20,15 +20,14 @@ function calcPropRadius(attValue) {
     var radius = Math.sqrt(area/Math.PI);
 
     return radius;
-    console.log(radius);
 };
 
-function createPropSymbols(data, map){
+function pointToLayer(feature, latlng, attributes,type){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+
     //create marker options
-    var attribute = "y1995";
-    
-    var geojsonMarkerOptions = {
-        radius: 8,
+    var options = {
         fillColor: "#ff7800",
         color: "#000",
         weight: 1,
@@ -36,28 +35,241 @@ function createPropSymbols(data, map){
         fillOpacity: 0.8
     };
 
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+    
+    if(type=="country"){
+    //build popup content string
+        var popupContent = "<p><b>Country:</b> " + feature.properties.Country;
+        popupContent += "<p><b>Number of U.S. bound Emigrants in 1990: </b> " + feature.properties[attribute];
+    } else {
+        var popupContent = "<p><b>Region:</b> " + feature.properties.Region;
+        popupContent += "<p><b>Number of U.S. bound Emigrants in 1990: </b> " + feature.properties[attribute];
+    }
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius) 
+    });
+    
+    //event listeners to open popup on hover
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        }
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes,type){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            var attValue = Number(feature.properties[attribute]);
-            console.log(attValue);
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-            return L.circleMarker(latlng, geojsonMarkerOptions);
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes,type);
         }
-    }).addTo(map);
+    }).addTo(mymap);
+};
+
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(map, attribute,type){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+            
+            //change the popup based on whether visualizing country or regional dat
+            if(type=="Country"){
+            //add city to popup content string
+            var popupContent = "<p><b>Country:</b> " + props.Country + "</p>";
+            } else {
+                var popupContent = "<p><b>Region:</b> " + props.Region + "</p>";
+            }
+            var year = attribute.substring(1,5);
+            
+            
+            popupContent +="<p><b>Number of U.S. bound Emigrants in " + year + ":</b> " + layer.feature.properties[attribute];
+
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+            
+        };
+    }); console.log("success");
+};
+
+
+function createSequenceControls(mymap,attributes,type){
+    //create range input element (slider)
+    $('#panel').html('<input class="range-slider" type="range">');
+    
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+      //below Example 3.4...add skip buttons
+    $('#panel').append('<button class="skip" id="reverse">Backward</button>');
+    $('#panel').append('<button class="skip" id="forward">Forward</button>');
+    
+    //Below Example 3.6 in createSequenceControls()
+    //Step 5: click listener for buttons
+    $('.skip').click(function(){
+        //sequence
+    });
+
+    //Step 5: input listener for slider
+    $('.range-slider').on('input', function(){
+        //sequence
+    }); 
+    
+    //Example 3.12 line 7...Step 5: input listener for slider
+    $('.range-slider').on('input', function(){
+        //Step 6: get the new index value
+        var index = $(this).val();
+        
+         //Called in both skip button and slider event listener handlers
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(mymap, attributes[index],type);
+    });
+
+ //Example 3.12 line 2...Step 5: click listener for buttons
+    $('.skip').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 6 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 6 : index;
+        };
+
+        //Step 8: update slider
+        $('.range-slider').val(index);
+        
+         //Called in both skip button and slider event listener handlers
+        //Step 9: pass new attribute to update symbols
+        updatePropSymbols(mymap, attributes[index],type);
+    });
+
+};
+
+//this function changes the data imported depending on what the user has selected
+function createRadioControls(mymap,attributes){
+    $('.form-radio').click(function(){
+    if(document.getElementById("region").checked){
+           mymap.eachLayer(function(layer){
+               if (layer.feature){
+                    mymap.removeLayer(layer);
+               }
+           }) 
+            getRegionalData(mymap);
+    }
+        
+    if(document.getElementById("country").checked){
+             mymap.eachLayer(function(layer){
+               if (layer.feature){
+                  mymap.removeLayer(layer);
+              }
+           })
+            getData(mymap);
+    }
+    })
+}
+
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+   //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute!="Country"){
+            attributes.push(attribute);
+        };
+    };
+    //console.log(attributes);
+    
+    return attributes;
+};
+
+//same as processData, slightly altered
+function processRegionalData(data){
+    //empty array to hold attributes
+    var attributes = [];
+   //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute!="Region"){
+            attributes.push(attribute);
+        };
+    };
+    //console.log(attributes);
+    
+    return attributes;
 };
 
 //Step 2: Import GeoJSON data
-function getData(map){
+function getData(mymap){
     //load the data
     $.ajax("data/LabData.geojson", {
         dataType: "json",
         success: function(response){
+            var type = "country";
+            var attributes = processData(response);
             //call function to create proportional symbols
-            createPropSymbols(response, map);
+            createPropSymbols(response, mymap, attributes,type);
+            createSequenceControls(mymap, attributes,type);
+            createRadioControls(mymap,attributes);
         }
     });
 };
 
+function getRegionalData(mymap){
+    //load the data
+    $.ajax("data/RegionalCoords.geojson", {
+        dataType: "json",
+        success: function(response){
+            //console.log("success");
+            var type = "region";
+            var attributes = processRegionalData(response);
+            //console.log(response);
+            //call function to create proportional symbols
+            createPropSymbols(response, mymap, attributes, type);
+            createSequenceControls(mymap, attributes,type);
+        }
+    }); 
+};
+
+   
 
 $(document).ready(getData(mymap));
